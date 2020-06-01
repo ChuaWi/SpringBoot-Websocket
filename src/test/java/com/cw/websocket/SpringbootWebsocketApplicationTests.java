1341d70d4071eb91bd3b77a88ce1ed0bb25a1f07
asus <2537394743@qq.com>
2020-06-01 12:04:38 +0800

    first commit

---
+++ src/test/java/com/cw/websocket/SpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cw.websocket;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
